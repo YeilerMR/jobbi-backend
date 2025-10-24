bbbee79fd2aa0c41f1931b31db67a6497ed97931
Jeycob Barrientos García <jeycobbarrientosgarcia@gmail.com>
2025-10-23 22:13:50 -0600

    Solucion de conflictos

--- index.js
+++ index.js
@@ -15,13 +15,10 @@ const specialtysRoutes = require('./src/modules/specialtys/specialtys.routes');
 const invitationRoutes = require('./src/modules/invitation/invitation.routes');
 const notificationsRoutes = require('./src/modules/notifications/notifications.routes');
 const appointmentsRoutes = require('./src/modules/appointments/appointments.routes');
-<<<<<<< HEAD
 const giftsRoutes = require("./src/modules/gift/gift.routes");
-=======
 //const appointmentsRoutes = require('./src/modules/appointments/appointments.routes'); // TODO: Create appointments module
 const employeesRoutes = require('./src/modules/employees/employees.routes');
 const usersRoutes = require('./src/modules/users/users.routes');
->>>>>>> aa7de40ac57a752e489990cd801fdaf4f0b0defa
 
 
 // --------------------------------------
