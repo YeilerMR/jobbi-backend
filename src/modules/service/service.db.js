ed89f86b553ea23c66e3adb63c4aa5092cd5627c
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2025-11-13 13:12:54 -0600

    Update service.db.js

--- src/modules/service/service.db.js
+++ src/modules/service/service.db.js
@@ -44,11 +44,11 @@ async function findServicesByBranchId(id_branch) {
 async function updateServiceById(id_service, updatedData) {
   const connection = await createConnection();
 
-  const { name, description, price, duration, state_service } = updatedData;
+  const { id_branch, id_specialty, name, description, price, duration, state_service } = updatedData;
 
   await connection.execute(
-    `UPDATE Service SET name = ?, description = ?, price = ?, duration = ?, state_service = ? WHERE id_service = ?`,
-    [name, description, price, duration, state_service, id_service]
+    `UPDATE Service SET id_branch = ?, id_specialty = ? , name = ?, description = ?, price = ?, duration = ?, state_service = ? WHERE id_service = ?`,
+    [id_branch, id_specialty, name, description, price, duration, state_service, id_service]
   );
 
   await connection.end();
